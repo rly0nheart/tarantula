17e6528f6d75372630c8d4923c37497a12f10ec9
Richard Mwewa <r1chardmw3wa@gmail.com>
2021-08-11 12:25:59 +0200

    Create BANNER.py

---
+++ assets/BANNER.py
@@ -0,0 +1,15 @@
+import os
+from assets.COLORS import *
+
+def banner():
+    os.system("clear")
+    print("""
+   ▄▄▄▄▀ ██   █▄▄▄▄ ██      ▄     ▄▄▄▄▀ ▄   █    ██   
+▀▀▀ █    █ █  █  ▄▀ █ █      █ ▀▀▀ █     █  █    █ █  
+    █    █▄▄█ █▀▀▌  █▄▄█ ██   █    █  █   █ █    █▄▄█ 
+   █     █  █ █  █  █  █ █ █  █   █   █   █ ███▄ █  █ 
+  ▀         █   █      █ █  █ █  ▀    █▄ ▄█     ▀   █ 
+           █   ▀      █  █   ██        ▀▀▀         █  
+          ▀          ▀                            ▀ %sv1.1.0%s
+rly0nheart - (https://github.com/rlyonheart)
+""" % (WHITE,RESET))
