5e080e8a8758a21e9678846545bac6f49eaee8c6
Richard Mwewa <74001397+rlyonheart@users.noreply.github.com>
2021-09-23 02:18:59 +0200

    Update COLORS.py

--- assets/COLORS.py
+++ assets/COLORS.py
@@ -1,18 +1,15 @@
-#!/usr/bin/env python3
 import sys
-import colorama
-from colorama import Back as bg
 
 colors = True
 machine = sys.platform # Detecting the os of current system
 if machine.lower().startswith(('os', 'win', 'darwin', 'ios')):
     colors = False # Colors will not be displayed in mac & windows
 if not colors:
-    RESET = GREEN = GRAY = YELLOW = RED = ""
+    RESET = RED = WHITE = GREEN = YELLOW = ''
 else:
-    colorama.init()
-    WHITE = colorama.Fore.WHITE
-    GRAY = colorama.Fore.LIGHTBLACK_EX
-    RESET = colorama.Fore.RESET
-    YELLOW = colorama.Fore.YELLOW
-    RED = colorama.Fore.RED
+    WHITE = "\x1b[97m"
+    GREEN = "\x1b[92m"
+    RED = "\x1b[91m"
+    RESET = "\x1b[0m"
+    YELLOW = "\x1b[93m"
+   
